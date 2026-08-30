94976217a39484caa82b1834db1a73ae10e3384c
misell-yay <9dmecellabunga17@gmail.com>
2026-08-30 20:41:35 +0700

    unggah pertama

---
+++ form.php
@@ -0,0 +1,14 @@
+<!-- form-get.html --> 
+<form action="proses.php" method="GET"> 
+<label>Cari Mahasiswa:</label> 
+<input type="text" name="keyword"> 
+<button type="submit">Cari</button> 
+</form> 
+<!-- form-post.html --> 
+<form action="login.php" method="POST"> 
+<label>Username:</label> 
+<input type="text" name="username"> 
+<label>Password:</label> 
+<input type="password" name="password"> 
+<button type="submit">Login</button> 
+</form> 
